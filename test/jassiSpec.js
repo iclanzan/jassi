e14944bb71cdfc05318b016cb93d9c6bd4a833b9
Sorin Iclanzan <sorin@iclanzan.com>
2013-12-28 17:12:40 -0800

    Exclude tests for unimplemented features.

--- test/jassiSpec.js
+++ test/jassiSpec.js
@@ -15,7 +15,7 @@ function testcaseHandler(nodeunit) {
 }
 
 
-grunt.file.expand({cwd: 'test'}, 'JSON-Schema-Test-Suite/tests/draft4/**/*.json').forEach(function (filepath) {
+grunt.file.expand({cwd: 'test'}, ['JSON-Schema-Test-Suite/tests/draft4/**/*.json', '!**/{format,zeroTerminatedFloats,definitions,ref,refRemote}.json']).forEach(function (filepath) {
   require(__dirname + '/' + filepath).forEach(function (testcase) {
     exports[testcase.description] = testcaseHandler.bind(testcase);
   });
